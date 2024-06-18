c65b4f2a4ff4ec2b40e5e1fbb1e7164ec1fe604a
Agbaje630 <agbajequdus630@gmail.com>
2024-06-18 13:56:18 +0100

    js practice

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function addTwoNumber () {
+    let numOne = 123
+    let numTwo = 456
+    let total = numOne + numTwo
+
+    return total
+
+}
+
+console.log (addTwoNumber())
+
+function greet (statementOne) {
+    return statementOne
+
+}
+console.log (greet( 'Ali is a boy'))
+
+function fname(firstName,lastName) {
+    console.log(firstName + lastName);
+}
+
+fname('Agbaje','Qudus');
